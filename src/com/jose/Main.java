3ab7ac34a495a3746e40dbc3ed6c054e528e7dab
joserdaw <joserdaw@gmail.com>
2017-04-25 13:39:55 +0200

    MSN
    donde esta

--- src/com/jose/Main.java
+++ src/com/jose/Main.java
@@ -6,6 +6,14 @@ public class Main {
 		
 		System.out.println("Hola mundo");
 		
+		boolean dsa=true;
+		int cont=0;
+		do{
+			cont++;
+			System.out.println("Dónde está la MSN?");
+			if(cont>5)dsa=false;
+		}while(dsa);
+		System.out.println("La MSN dónde está");
 	}
 
 }
