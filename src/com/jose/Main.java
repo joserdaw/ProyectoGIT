0f35f242dc39c2f5fbae89211bc69476d2cab102
joserdaw <joserdaw@gmail.com>
2017-04-25 13:40:42 +0200

    bucle
    2

--- src/com/jose/Main.java
+++ src/com/jose/Main.java
@@ -11,6 +11,11 @@ public class Main {
 		do{
 			cont++;
 			System.out.println("Dónde está la MSN?");
+			for(int x=0;x<2;x++){
+				
+				System.out.println("Dónde está");
+				
+			}
 			if(cont>5)dsa=false;
 		}while(dsa);
 		System.out.println("La MSN dónde está");
